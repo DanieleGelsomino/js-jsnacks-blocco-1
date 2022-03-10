6cbc9084ad92b01859c6093b95521bf0d7233a4b
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-10 17:30:02 +0100

    add jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,3 +4,17 @@ console.log("JS OK!");
 /* consegna:
    Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. 
 */
+
+const inputNumero = prompt("Digita un numero contenente 4 cifre");
+const cifra = inputNumero;
+const sommaCifre =
+  parseInt(cifra[0]) +
+  parseInt(cifra[1]) +
+  parseInt(cifra[2]) +
+  parseInt(cifra[3]);
+if (isNaN(inputNumero) || !(inputNumero.length == 4)) {
+  alert("Inserisci 4 cifre!");
+} else {
+  console.log("Hai inserito correttamente!");
+  console.log("la somma delle tue cifre è: " + sommaCifre);
+}
