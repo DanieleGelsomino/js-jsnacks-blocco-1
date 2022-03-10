6e5dd3d0a5849f5e59c0dbe4f794e892d7d2f161
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-10 16:09:24 +0100

    add jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -5,3 +5,18 @@ console.log("JS OK!");
    In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
    chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 */
+
+const invitedList = ["danny", "john", "jack", "emily"];
+
+const nameInvited = prompt("Inserisci il tuo nome");
+
+let confirm = false;
+
+for (let i = 0; i < invitedList.length; i++) {
+  if (nameInvited.toLowerCase() === invitedList[i]) {
+    confirm = true;
+    console.log("Puoi entrare");
+  } else {
+    console.log("Non puoi entrare");
+  }
+}
