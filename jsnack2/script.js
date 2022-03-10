7e6a8164712ed8b2bcf5dfeb220ca7407ebbb166
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-10 16:13:38 +0100

    add jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,3 +5,14 @@ console.log("JS OK!");
    Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero,
    se è dispari inseriscilo nell’array. 
 */
+
+const containerNumber = [];
+
+for (let i = 0; i < 6; i++) {
+  const number = prompt("inserisci un numero");
+  console.log(number);
+  if (number % 2 !== 0) {
+    containerNumber.push(number);
+  }
+  console.log(containerNumber);
+}
